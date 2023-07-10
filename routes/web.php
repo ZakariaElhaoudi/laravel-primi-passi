e67977c015296997915e8b11d7a0ce89288e3414
Zakaria96 <zakarialavoro@gmail.com>
2023-07-10 15:57:38 +0200

    milestone 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $hello = "Hello world!!";
+    return view('home', compact('hello'));
 });
